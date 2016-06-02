c5605385ccc604da39393c2338864f9ee2ec739a
Fabien AMICO <f.amico@treeptik.fr>
2016-06-02 15:53:12 +0200

    add container name to intro

---
+++ src/main/java/org/springframework/webflow/samples/booking/ContainerConf.java
@@ -0,0 +1,18 @@
+package org.springframework.webflow.samples.booking;
+
+public class ContainerConf {
+
+	private String hostName;
+	
+	public ContainerConf() {
+	}
+
+	public String getHostName() {
+		return hostName;
+	}
+
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+	
+}
